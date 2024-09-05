9e4503da437c553252573b1834db2c0b33ecc69d
Polina Chetnikova <38739507+pchova@users.noreply.github.com>
2024-09-05 14:33:05 -0700

    first number input is assigned to var number1 after user clicks an operator

--- app.js
+++ app.js
@@ -22,32 +22,21 @@ function clearDisplay() {
 }
 
 function inputNumber(number) {
-    if (number === '.' && displayValue.includes('.')) return;
+    if (number === '.' && displayValue.includes('.')) return; 
 
-    if (displayValue === '0' ) {
+    if (displayValue === '0') {
         displayValue = number;
     } else {
         displayValue += number;
     }
     updateDisplay();
 
-    number1 = displayValue;
-    console.log("number1 is " + number1);
-
-    /*
-    All we need to do is be able to assign number1 and number2 to store in a variable and then it will work!!
-    
-    May need to use a boolean flag to flag when operator has been chosen, then we can split the numbers accordingly
-    Maybe when user hits equals, it actually calls another function to split the displayValue string into 3 variables
-    and then calls operate()
-
-    if displayValue.includes('x' || '&divide;' || '+' || '-') {
-        number1 = number1.split(0, operator);
-        number2 = number2.split(operator, -1);
-        } 
-
-    */
+    if(isOperatorClicked === true) {
+        array = displayValue.split(operator);
+        number1 = array[0];
+    }
 }
+
 
 function inputOperator(mathoperation) {
     if (operator !== "") return;
@@ -56,12 +45,46 @@ function inputOperator(mathoperation) {
         operator = mathoperation;
         displayValue += mathoperation;
         isOperatorClicked = true;
-        console.log("mathoperation and boolean: " + operator + " " + isOperatorClicked);
     } 
-
     updateDisplay();
-    //console.log("operator is " + operator);
 }
+
+function operate (num1, num2, operator) {
+    console.log("number1 = " + number1);
+    console.log("number2 = " + number2);
+    console.log("operator = " + operator);
+
+    switch (operator) {
+        case "&#43;":
+            displayValue.textContent = add(parseFloat(num1), parseFloat(num2));
+        case "&#8722;":
+            return subtract(num1, num2);
+        case "&times;":
+            return multiply(num1, num2);
+        case "&divide;":
+            return divide(num1, num2);
+        default:
+            return alert("Error in calculation.");
+    }
+}
+
+/* MATH FUNCTIONS */
+function add(a,b) {
+    return (a + b).toFixed(3);
+}
+
+function subtract(a,b) {
+    return (a - b).toFixed(3);
+}
+
+function multiply (a,b) {
+    return (a * b).toFixed(3);
+}
+
+function divide (a,b) {
+    return (a / b).toFixed(3);
+}
+
 
 function buttonClick(event) {
     const target = event.target;
@@ -90,41 +113,4 @@ function buttonClick(event) {
 buttons.forEach(button => {
     button.addEventListener("click", buttonClick);
 });
-
-
-/* MATH FUNCTIONS */
-function add(a,b) {
-    return (a + b).toFixed(3);
-}
-
-function subtract(a,b) {
-    return (a - b).toFixed(3);
-}
-
-function multiply (a,b) {
-    return (a * b).toFixed(3);
-}
-
-function divide (a,b) {
-    return (a / b).toFixed(3);
-}
-
-function operate (num1, num2, operator) {
-    console.log("number1 = " + number1);
-    console.log("number2 = " + number2);
-    console.log("operator = " + operator);
-
-    switch (operator) {
-        case "&#43;":
-            displayValue.textContent = add(parseFloat(num1), parseFloat(num2));
-        case "&#8722;":
-            return subtract(num1, num2);
-        case "&times;":
-            return multiply(num1, num2);
-        case "&divide;":
-            return divide(num1, num2);
-        default:
-            return alert("Error in calculation.");
-    }
-}
 
